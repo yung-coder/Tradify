d1afb2c82fa82e0027ba8875747c0061d859db5d
yung-coder <chandel.sanjeev5@gmail.com>
2022-09-30 00:32:41 +0530

    checking

--- trade-app/pages/index.js
+++ trade-app/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Check</h1>
+      <h1 className="bg-red-400">Check</h1>
     </div>
   );
 }
