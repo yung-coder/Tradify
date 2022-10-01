c522aa3f8cce58f90983c75e87a3da8752542f17
yung-coder <chandel.sanjeev5@gmail.com>
2022-10-01 16:06:28 +0530

    init

--- trade-app/components/Navbar.jsx
+++ trade-app/components/Navbar.jsx
@@ -46,10 +46,10 @@ const Navbar = () => {
           <div
             class={` ${
               drop ? "block" : "hidden"
-            }  justify-between items-center w-full md:flex md:w-auto md:order-1`}
+            }  justify-center items-center w-full md:flex md:w-auto md:order-1`}
             id="mobile-menu-2"
           >
-            <ul class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg   md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 bg-transparent">
+            <ul class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg   md:flex-row md:space-x-14 md:mt-0 md:text-sm md:font-medium md:border-0 bg-transparent">
               <li>
                 <a
                   href="#"
