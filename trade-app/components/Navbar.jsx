4f5cebf3831ca055164d3bb7fc3eef296f2e5999
yung-coder <chandel.sanjeev5@gmail.com>
2022-10-01 15:47:05 +0530

    nav done

--- trade-app/components/Navbar.jsx
+++ trade-app/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   const [drop, setdrop] = useState(false);
   return (
     <div>
-      <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+      <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900">
         <div class="container flex flex-wrap justify-between items-center mx-auto">
           <a href="https://flowbite.com/" class="flex items-center">
             <img
@@ -13,7 +13,7 @@ const Navbar = () => {
               alt="Flowbite Logo"
             />
             <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
+              Tradify
             </span>
           </a>
           <div class="flex items-center md:order-2">
@@ -49,7 +49,7 @@ const Navbar = () => {
             }  justify-between items-center w-full md:flex md:w-auto md:order-1`}
             id="mobile-menu-2"
           >
-            <ul class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg   md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 bg-transparent">
               <li>
                 <a
                   href="#"
