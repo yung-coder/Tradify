bc97457470516067642c2b60f87ecdf73c2b7ef2
yung-coder <chandel.sanjeev5@gmail.com>
2022-10-02 23:52:38 +0530

    coin info

--- trade-app/pages/coin/[id].js
+++ trade-app/pages/coin/[id].js
@@ -1,38 +1,78 @@
-import axios from 'axios';
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react';
-import { CryptoState } from '../../AppContext';
-import { SingleCoin } from '../../utils/endpoints';
-
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { CryptoState } from "../../AppContext";
+import { SingleCoin } from "../../utils/endpoints";
+import ReactHtmlParser from "react-html-parser";
+import { numberWithCommas } from "../../components/Cointable";
 const Post = () => {
-//   const router = useRouter()
-//   const { slug } = router.query
+  //   const router = useRouter()
+  //   const { slug } = router.query
   const { query } = useRouter();
   const [coin, setcoin] = useState();
-  const {currency , setcurrency} = CryptoState()
+  const { currency, symbol } = CryptoState();
 
-  const fetchcoin = async () =>{
-     const {data} = await axios.get(SingleCoin(query.id));
+  const fetchcoin = async () => {
+    const { data } = await axios.get(SingleCoin(query.id));
 
-     setcoin(data);
-  }
+    setcoin(data);
+  };
 
   useEffect(() => {
     fetchcoin();
-  }, [])
+  }, []);
 
   console.log(coin);
-  
 
+  if (!coin) {
+    return <p>Loading...</p>;
+  }
   return (
-    <div>
-        <div >
-            
+    <div className="flex flex-col items-center md:flex-row">
+      <div>
+        <div className="flex flex-col justify-between items-center">
+          <img src={coin?.image.large} alt="" />
+          <h1 className="text-4xl font-bold">{coin?.name}</h1>
         </div>
 
-        {/* chart */}
-    </div>
-  )
-}
+        <div className="break-words font-semibold ">
+          {ReactHtmlParser(coin?.description.en.split(". ")[0])}.
+        </div>
 
-export default Post
+        <div>
+          <div className="flex-col justify-between">
+            <div className="flex space-x-2">
+              <h1>RANK</h1>
+              <span>{coin?.market_cap_rank}</span>
+            </div>
+            <div className="flex space-x-2">
+              <h1>CURRENT PRICE</h1>
+              <span>
+                {symbol}{" "}
+                {numberWithCommas(
+                  coin?.market_data.current_price[currency.toLowerCase()]
+                )}
+              </span>
+            </div>
+            <div className="flex space-x-2">
+              <h1>MARKET CAP</h1>
+              <span>
+                {symbol}{" "}
+                {numberWithCommas(
+                  coin?.market_data.market_cap[currency.toLowerCase()]
+                    .toString()
+                    .slice(0, -6)
+                )}
+                M
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* chart */}
+    </div>
+  );
+};
+
+export default Post;
