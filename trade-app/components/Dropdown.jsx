0089733db345f83d26816721999dd3c1edaa5234
yung-coder <chandel.sanjeev5@gmail.com>
2022-10-02 18:19:02 +0530

    fixies

--- trade-app/components/Dropdown.jsx
+++ trade-app/components/Dropdown.jsx
@@ -1,77 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Dropdown = () => {
+  const [drop, setdrop] = useState(false);
   return (
-    <div>
-      <button
-        id="dropdownDefault"
-        data-dropdown-toggle="dropdown"
-        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        type="button"
-      >
-        Dropdown button{" "}
-        <svg
-          class="ml-2 w-4 h-4"
-          aria-hidden="true"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M19 9l-7 7-7-7"
-          ></path>
-        </svg>
-      </button>
-      <div
-        id="dropdown"
-        class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700"
-        data-popper-reference-hidden=""
-        data-popper-escaped=""
-        data-popper-placement="bottom"
-      >
-        <ul
-          class="py-1 text-sm text-gray-700 dark:text-gray-200"
-          aria-labelledby="dropdownDefault"
-        >
-          <li>
-            <a
-              href="#"
-              class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Dashboard
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Settings
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Earnings
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Sign out
-            </a>
-          </li>
-        </ul>
+    <nav class="p-4  bg-black md:p-2">
+      <div class="container flex  justify-end items-end">
+        <div class="w-full md:block md:w-auto" id="mobile-menu">
+          <ul class="flex flex-col  rounded-lg  md:flex-row md:mt-0 md:text-sm md:font-medium ">
+            <li>
+              <button
+                id="dropdownNavbarLink"
+                data-dropdown-toggle="dropdownNavbar"
+                class="flex justify-between items-center rounded bg-transparent text-white"
+                onClick={() => {
+                  setdrop(!drop);
+                }}
+              >
+                Dropdown{" "}
+                <svg
+                  class="ml-1 w-5 h-5"
+                  aria-hidden="true"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </button>
+
+              <div
+                id="dropdownNavbar"
+                class={`${
+                  drop ? "block" : "hidden"
+                } z-10 w-44 font-normal bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600 absolute mt-5`}
+              >
+                <ul
+                  class="py-1 text-sm text-gray-700 dark:text-gray-400"
+                  aria-labelledby="dropdownLargeButton"
+                >
+                  <li>
+                    <a
+                      href="#"
+                      class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      Dashboard
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      Settings
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      Earnings
+                    </a>
+                  </li>
+                </ul>
+                <div class="py-1">
+                  <a
+                    href="#"
+                    class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white"
+                  >
+                    Sign out
+                  </a>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
--- trade-app/pages/CryptoTracker.jsx
+++ trade-app/pages/CryptoTracker.jsx
@@ -4,24 +4,15 @@ import Banner from "../images/temp.jpg";
 const CryptoTracker = () => {
   return (
     <div className="flex flex-col justify-center items-center">
-      <div className="w-full">
-        <nav class="relative w-full flex flex-wrap items-center justify-between py-3 bg-black text-white">
-          <div class="container-fluid w-full flex flex-wrap items-center justify-between px-6">
-            <div class="container-fluid ">
-              <a class="text-xl " href="#">
-                Navbar
-              </a>
-            </div>
-              <Dropdown />
-          </div>
-        </nav>
-      </div>
-      <div className=" border-red-500 border">
+      <div className="w-full border-red-500 border">
         <img
           src="https://thumbs.dreamstime.com/b/ethereum-cryptocurrency-futuristic-style-cyber-digital-coin-eth-banner-website-presentation-computer-circuit-board-220393721.jpg"
           alt=""
-          className="md:w-full md:h-[400px]"
+          className="md:w-full md:h-[80vh]"
         />
+      </div>
+      <div className="w-full">
+        <Dropdown />
       </div>
     </div>
   );
