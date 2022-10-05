57e1420cd47903811ac7443eae7c28a1474b83ed
yung-coder <chandel.sanjeev5@gmail.com>
2022-10-05 13:58:18 +0530

    news raw

--- trade-app/AppContext.js
+++ trade-app/AppContext.js
@@ -1,19 +1,35 @@
+import axios from 'axios';
 import React, {  createContext, useContext, useEffect, useState } from 'react'
-
 const Crypto = createContext()
 
 const AppContext = ({children}) => {
 
     const [currency,setcurrency] = useState("INR");
     const [symbol,setsymbol] = useState("₹");
+    const [News, setNews] = useState([]);
 
     useEffect(() => {
        if(currency === "INR") setsymbol("₹")
        else if(currency === "USD") setsymbol("$")
     }, [currency])
+
+    const getCryptoNews =() => {
+      axios(`https://newsdata.io/api/1/news?apikey=pub_11954a4c7647fd461e2c81c73dbfccf3b18c8&q=crypto&language=en&category=business`, {
+       }).then(response => {
+          console.log(response.data);
+          setNews(response.data.results);
+       })
+    }
+    const getStocknews =() => {
+      axios(`https://newsdata.io/api/1/news?apikey=pub_11954a4c7647fd461e2c81c73dbfccf3b18c8&q=stock&language=en&category=business`, {
+       }).then(response => {
+          console.log(response.data);
+          setNews(response.data.results);
+       })
+    }
     
   return (
-     <Crypto.Provider value={{currency,symbol , setcurrency}}>
+     <Crypto.Provider value={{currency,symbol , setcurrency , getCryptoNews , News , getStocknews }}>
         {children}
      </Crypto.Provider>
   )
@@ -23,5 +39,8 @@ export default AppContext
 
 
 export const CryptoState=()=>{
-    return useContext(Crypto)
+    return(
+      useContext(Crypto)
+    )
+      
 }
---
+++ trade-app/pages/News.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { CryptoState } from "../AppContext";
+import Newscard from "../components/Newscard";
+
+const News = () => {
+  const { getCryptoNews, News , getStocknews } = CryptoState();
+  return (
+    <div>
+      <div className="flex flex-col">
+        <div className="flex justify-center items-center">
+          <div className="flex space-x-5 mt-6">
+            <button className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 " onClick={getStocknews}>
+              Stock News
+            </button>
+            <button className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" onClick={getCryptoNews}>
+              Crypto News
+            </button>
+          </div>
+        </div>
+        <div className="flex
+         flex-col justify-center items-center mt-16 space-y-14">
+            {News.map((cont)=>{
+                return (
+                    <Newscard title={cont.title} desc={cont.description} link={cont.link}/>
+                )
+            })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default News;
