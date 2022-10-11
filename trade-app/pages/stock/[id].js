626a3c7a304e1f1c7f4ecc738070dbf75770b16d
yung-coder <chandel.sanjeev5@gmail.com>
2022-10-11 20:31:20 +0530

    modify

--- trade-app/pages/stock/[id].js
+++ trade-app/pages/stock/[id].js
@@ -24,9 +24,28 @@ const Post = () => {
       {details.map((item) => {
         return (
           <div key={item.symbol}>
-            <div>
-              <img src={item.image} alt="" />
-               <h1>{item.sector}</h1>
+            <div className="flex flex-col space-y-6">
+              <div className="flex flex-col space-y-6 justify-center items-center">
+                <img src={item.image} alt="" />
+                <h1 className="text-xl font-bold">{item.sector}</h1>
+              </div>
+              <div className="flex flex-col justify-center items-center space-y-6 font-semibold">
+                <div className="flex space-x-5">
+                  <p>{item.companyName}</p>
+                  <p>{item.volAvg}</p>
+                </div>
+                <div className="flex space-x-5">
+                  <p>{item.price}</p>
+                  <p>{item.mktCap}</p>
+                </div>
+                <div>
+                  <a href={item.website} className='hover:text-blue-700'>Website ➡️</a>
+                </div>
+              </div>
+              <div className="w-96 border p-3 text-center font-medium flex flex-col space-y-6">
+                 <h1 className="text-2xl font-bold">About</h1>
+                <p>{item.description}</p>
+              </div>
             </div>
           </div>
         );
