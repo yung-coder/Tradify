9fe4804a5d88c421461f4856aeff830c1e37ff98
yung-coder <chandel.sanjeev5@gmail.com>
2022-10-14 23:35:34 +0530

    fixes

--- trade-app/pages/News.jsx
+++ trade-app/pages/News.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { CryptoState } from "../AppContext";
 import Newscard from "../components/Newscard";
 
 const News = () => {
   const { getCryptoNews, News , getStocknews } = CryptoState();
+  useEffect(() => {
+     getCryptoNews();
+  }, [])
+  
   return (
     <div>
-      <div className="flex flex-col">
+      <div className="flex flex-col  bg-[#eaf4fc]">
         <div className="flex justify-center items-center">
           <div className="flex space-x-5 mt-6">
             <button className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 " onClick={getStocknews}>
@@ -18,7 +22,7 @@ const News = () => {
           </div>
         </div>
         <div className="flex
-         flex-col justify-center items-center mt-16 space-y-14">
+         flex-col justify-center items-center mt-16 space-y-14 p-4">
             {News.map((cont)=>{
                 return (
                     <Newscard title={cont.title} desc={cont.description} link={cont.link} key={cont.title}/>
