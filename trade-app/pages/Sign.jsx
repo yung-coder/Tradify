d8f19051d00e8acb8b6116cc09034ed3c9ca22d4
yung-coder <chandel.sanjeev5@gmail.com>
2022-10-16 18:19:31 +0530

    signin

--- trade-app/pages/Sign.jsx
+++ trade-app/pages/Sign.jsx
@@ -32,149 +32,91 @@ const SignIn = () => {
   const disableForm = isLoading || needsEmailVerification;
 
   return (
-    // <div>
-    //   <div>
-    //     {needsEmailVerification ? (
-    //       <p>
-    //         Please check your mailbox and follow the verification link to verify
-    //         your email.
-    //       </p>
-    //     ) : (
-    //       <>
-    //         <form onSubmit={handleOnSubmit}>
-    //           <input
-    //             type="email"
-    //             label="Email address"
-    //             value={email}
-    //             onChange={(e) => setEmail(e.target.value)}
-    //             disabled={disableForm}
-    //             required
-    //           />
-    //           <input
-    //             type="password"
-    //             label="Password"
-    //             value={password}
-    //             onChange={(e) => setPassword(e.target.value)}
-    //             disabled={disableForm}
-    //             required
-    //           />
-
-    //           <button type="submit" disabled={disableForm}>
-    //             {isLoading ? 'Loading': "Sign in"}
-    //           </button>
-
-    //           {isError ? <p>{error?.message}</p> : null}
-    //         </form>
-    //       </>
-    //     )}
-    //   </div>
-
-    //   <p>
-    //     No account yet?{" "}
-    //     <Link href="/sign-up">
-    //       <a>Sign up</a>
-    //     </Link>
-    //   </p>
-    // </div>
-
-    <section class="bg-gray-50 dark:bg-gray-900">
-      <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <a
-          href="#"
-          class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
-        >
-          <img
-            class="w-8 h-8 mr-2"
-            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
-            alt="logo"
-          />
-          Flowbite
-        </a>
-        <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Sign in to your account
-            </h1>
-            <form class="space-y-4 md:space-y-6" action="#">
-              <div>
-                <label
-                  for="email"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Your email
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="name@company.com"
-                  required=""
-                />
-              </div>
-              <div>
-                <label
-                  for="password"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Password
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="••••••••"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
-                />
-              </div>
-              <div class="flex items-center justify-between">
-                <div class="flex items-start">
-                  <div class="flex items-center h-5">
-                    <input
-                      id="remember"
-                      aria-describedby="remember"
-                      type="checkbox"
-                      class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                      required=""
-                    />
-                  </div>
-                  <div class="ml-3 text-sm">
-                    <label
-                      for="remember"
-                      class="text-gray-500 dark:text-gray-300"
+    <div>
+      <div>
+        {needsEmailVerification ? (
+          <p>
+            Please check your mailbox and follow the verification link to verify
+            your email.
+          </p>
+        ) : (
+          <>
+            <section class="bg-[#2AAA8A]">
+              <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
+                <a class="flex items-center  text-3xl mb-6 font-bold text-gray-900 dark:text-white italic">
+                  Tradify
+                </a>
+                <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                  <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                      Sign in to your account
+                    </h1>
+                    <form
+                      class="space-y-4 md:space-y-6"
+                      action="#"
+                      onSubmit={handleOnSubmit}
                     >
-                      Remember me
-                    </label>
+                      <div>
+                        <label
+                          for="email"
+                          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                        >
+                          Your email
+                        </label>
+                        <input
+                          type="email"
+                          name="email"
+                          id="email"
+                          class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                          placeholder="name@company.com"
+                          onChange={(e) => setEmail(e.target.value)}
+                          disabled={disableForm}
+                          required
+                        />
+                      </div>
+                      <div>
+                        <label
+                          for="password"
+                          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                        >
+                          Password
+                        </label>
+                        <input
+                          type="password"
+                          name="password"
+                          id="password"
+                          placeholder="••••••••"
+                          class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                          onChange={(e) => setPassword(e.target.value)}
+                          disabled={disableForm}
+                          required
+                        />
+                      </div>
+                      <button
+                        type="submit"
+                        class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                        disabled={disableForm}
+                      >
+                        {isLoading ? "Loading" : "Sign in"}
+                      </button>
+                      <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                        Don’t have an account yet?{" "}
+                        <a
+                          href="#"
+                          class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                        >
+                          Sign up
+                        </a>
+                      </p>
+                    </form>
                   </div>
                 </div>
-                <a
-                  href="#"
-                  class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
-                >
-                  Forgot password?
-                </a>
               </div>
-              <button
-                type="submit"
-                class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-              >
-                Sign in
-              </button>
-              <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                Don’t have an account yet?{" "}
-                <a
-                  href="#"
-                  class="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                >
-                  Sign up
-                </a>
-              </p>
-            </form>
-          </div>
-        </div>
+            </section>
+          </>
+        )}
       </div>
-    </section>
+    </div>
   );
 };
 
