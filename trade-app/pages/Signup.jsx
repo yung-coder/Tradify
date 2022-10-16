35a784810e13f42222e377a255b0de082cf635d7
yung-coder <chandel.sanjeev5@gmail.com>
2022-10-16 19:05:52 +0530

    signuo

--- trade-app/pages/Signup.jsx
+++ trade-app/pages/Signup.jsx
@@ -1,107 +1,167 @@
-import { useState } from 'react'
-import { useRouter } from 'next/router'
-import { useSignUpEmailPassword } from '@nhost/nextjs'
-import Link from 'next/link'
-import Image from 'next/image'
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { useSignUpEmailPassword } from "@nhost/nextjs";
+import Link from "next/link";
+import Image from "next/image";
 
 const SignUp = () => {
-  const [firstName, setFirstName] = useState('')
-  const [lastName, setLastName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const router = useRouter()
+  const router = useRouter();
 
-  const { signUpEmailPassword, isLoading, isSuccess, needsEmailVerification, isError, error } =
-    useSignUpEmailPassword()
+  const {
+    signUpEmailPassword,
+    isLoading,
+    isSuccess,
+    needsEmailVerification,
+    isError,
+    error,
+  } = useSignUpEmailPassword();
 
   const handleOnSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     await signUpEmailPassword(email, password, {
       displayName: `${firstName} ${lastName}`.trim(),
       metadata: {
         firstName,
-        lastName
-      }
-    })
-  }
+        lastName,
+      },
+    });
+  };
 
   if (isSuccess) {
-    console.log('Done in')
-    router.push('/')
-    return null
+    console.log("Done in");
+    router.push("/");
+    return null;
   }
 
-  const disableForm = isLoading || needsEmailVerification
+  const disableForm = isLoading || needsEmailVerification;
 
   return (
-    <div className='border border-red-500 mt-16' >
-      <div >
-        {/* <div >
-          <Image src="/logo.svg" alt="logo" layout="fill" objectFit="contain" />
-        </div> */}
-
+    <div>
+      <div>
         {needsEmailVerification ? (
-          <p >
-            Please check your mailbox and follow the verification link to verify your email.
+          <p>
+            Please check your mailbox and follow the verification link to verify
+            your email.
           </p>
         ) : (
-          <form onSubmit={handleOnSubmit} className=''>
-            <div>
-              <input
-                label="First name"
-                value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
-                disabled={disableForm}
-               
-                className='bg-[#232B2B] text-white rounded-md outline-none p-2 placeholder:text-white'
-              />
-              <input
-                label="Last name"
-                value={lastName}
-                onChange={(e) => setLastName(e.target.value)}
-                disabled={disableForm}
-                required
-                className='bg-[#232B2B] text-white rounded-md outline-none p-2 placeholder:text-white'
-              />
+          <section class="bg-[#2AAA8A]">
+            <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
+              <a
+                href="#"
+                class="flex items-center mb-6 text-3xl font-semibold text-gray-900 dark:text-white italic"
+              >
+                Tradify
+              </a>
+              <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                  <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                    Create account
+                  </h1>
+                  <form class="space-y-4 md:space-y-6" action="#"  onSubmit={handleOnSubmit}>
+                    <div>
+                      <label
+                        for="name"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >
+                        First Name
+                      </label>
+                      <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="jhon"
+                        onChange={(e) => setFirstName(e.target.value)}
+                        disabled={disableForm}
+                        required
+                      />
+                    </div>
+                    <div>
+                      <label
+                        for="name"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >
+                        Last Name
+                      </label>
+                      <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Byard"
+                        onChange={(e) => setLastName(e.target.value)}
+                        disabled={disableForm}
+                        required
+                      />
+                    </div>
+                    <div>
+                      <label
+                        for="email"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >
+                        Email
+                      </label>
+                      <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        placeholder="name@gmail.com"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        onChange={(e) => setEmail(e.target.value)}
+                        disabled={disableForm}
+                        required
+                      />
+                    </div>
+                    <div>
+                      <label
+                        for="password"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >
+                        password
+                      </label>
+                      <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        placeholder="••••••••"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        onChange={(e) => setPassword(e.target.value)}
+                        disabled={disableForm}
+                        required
+                      />
+                    </div>
+                    <button
+                      type="submit"
+                      class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                      disabled={disableForm}
+                    >
+                      {isLoading ? "Loading" : "Create account"}
+                    </button>
+                    <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                      Already have an account?{" "}
+                      <a
+                        href="#"
+                        class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                      >
+                        Login here
+                      </a>
+                    </p>
+                  </form>
+                </div>
+              </div>
             </div>
-            <input
-              type="email"
-              label="Email address"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              disabled={disableForm}
-              required
-              className='bg-[#232B2B] text-white rounded-md outline-none p-2 placeholder:text-white'
-            />
-            <input
-              type="password"
-              label="Create password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              disabled={disableForm}
-              required
-              className='bg-[#232B2B] text-white rounded-md outline-none p-2 placeholder:text-white'
-            />
-
-            <button type="submit" disabled={disableForm}>
-              {isLoading ? 'Loading' : 'Create account'}
-            </button>
-
-            {isError ? <p > {error?.message}</p> : null}
-          </form>
+          </section>
         )}
+         {isError ? <p> {error?.message}</p> : null}
       </div>
-
-      <p >
-        Already have an account?{' '}
-        <Link href="/Sign">
-          <a>Sign in</a>
-        </Link>
-      </p>
     </div>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
