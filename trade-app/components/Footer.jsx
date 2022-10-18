dcf8840f853feb2d3c8eda709b75ddc8b9f448e8
yung-coder <chandel.sanjeev5@gmail.com>
2022-10-18 20:41:24 +0530

    init

--- trade-app/components/Footer.jsx
+++ trade-app/components/Footer.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-      <footer class="p-2  shadow md:flex md:items-center md:justify-between md:p-4 bg-[#98FB98] text-black">
+      <footer class="p-1  shadow md:flex md:items-center md:justify-between md:p-4 bg-[#98FB98] text-black">
         <span class="text-sm  sm:text-center ">
           © 2022{" "}
-          <a href="https://flowbite.com/" class="hover:underline">
-            Flowbite™
+          <a href="https://github.com/yung-coder/Tradify" class="hover:underline">
+             Tradify
           </a>
           . All Rights Reserved.
         </span>
